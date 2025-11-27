7db7f4630e6d824fd137a53106898520a07a8021
Luccavco <lucca.vco@puccampinas.edu.br>
2025-11-27 15:36:38 -0300

    fixes integration tests

--- tests/integration/shortener.integration.test.js
+++ tests/integration/shortener.integration.test.js
@@ -2,27 +2,24 @@ const request = require("supertest");
 const { createApp, createPoolFromEnv } = require("../../index");
 
 describe("Integração: comportamento do encurtador", () => {
-  let app, pool;
+  let app;
+  let pool;
 
-  beforeAll(async () => {
+  beforeAll(() => {
     pool = createPoolFromEnv();
     app = createApp(pool);
   });
 
   afterAll(async () => {
+    await pool.query("DELETE FROM urls WHERE url LIKE 'https://test.com/%'");
     await pool.end();
   });
 
-  test("Não deve aceitar URL inválida", async () => {
-    const res = await request(app).post("/encurtar").send({ url: "aaaaaaa" });
-    expect(res.status).toBe(400);
-  });
-
   test("Encurtar a mesma URL retorna códigos diferentes", async () => {
-    const url = "https://youtube.com";
+    const url = "https://test.com/repeated";
 
-    const r1 = await request(app).post("/encurtar").send({ url });
-    const r2 = await request(app).post("/encurtar").send({ url });
+    const r1 = await request(app).post("/encurtar").send({ longUrl: url });
+    const r2 = await request(app).post("/encurtar").send({ longUrl: url });
 
     expect(r1.body.shortUrl).not.toBe(r2.body.shortUrl);
   });
--- tests/integration/url.integration.test.js
+++ tests/integration/url.integration.test.js
@@ -2,27 +2,33 @@ const request = require("supertest");
 const { createApp, createPoolFromEnv } = require("../../index");
 
 describe("Integração: criação e redirecionamento de URL curta", () => {
-  let app, pool;
+  let app;
+  let pool;
 
-  beforeAll(async () => {
+  beforeAll(() => {
     pool = createPoolFromEnv();
     app = createApp(pool);
   });
 
   afterAll(async () => {
+    await pool.query("DELETE FROM urls WHERE url LIKE 'https://test-case.com/%'");
     await pool.end();
   });
 
   test("Deve criar uma URL curta e redirecionar corretamente", async () => {
-    const originalUrl = "https://google.com";
+    const originalUrl = "https://test-case.com/example";
 
-    const create = await request(app).post("/encurtar").send({ url: originalUrl });
+    const create = await request(app)
+      .post("/encurtar")
+      .send({ longUrl: originalUrl });
+
     expect(create.status).toBe(201);
     expect(create.body.shortUrl).toBeDefined();
 
     const code = create.body.shortUrl.split("/").pop();
 
-    const redirect = await request(app).get(`/${code}`).redirects(0);
+    const redirect = await request(app).get(`/${code}`);
+
     expect(redirect.status).toBe(302);
     expect(redirect.headers.location).toBe(originalUrl);
   });
